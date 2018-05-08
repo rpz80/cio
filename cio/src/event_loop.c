5413485c91264184be80b4bdf7f35153d1504081
rpz80 <rippa80@gmail.com>
2018-05-08 09:27:01 +0300

    Start with event_loop implementation

--- cio/src/event_loop.c
+++ cio/src/event_loop.c
@@ -1,23 +1,33 @@
 #include "event_loop.h"
 #include "pollset.h"
+#include <stdlib.h>
 
 struct _event_loop {
     void *pollset;
+    int need_stop;
 };
 
 void *cio_new_event_loop()
 {
-
+    struct _event_loop *result = malloc(sizeof(struct _event_loop));
+    result->pollset = cio_new_pollset();
+    result->need_stop = 0;
+    return result;
 }
 
 void cio_free_event_loop(void *loop)
 {
-
+    struct _event_loop *result = (struct _event_loop *) loop;
+    cio_free_pollset(result->pollset);
+    free(loop);
 }
 
 int cio_event_loop_start(void *loop)
 {
-
+    struct _event_loop *l = (struct _event_loop *) loop;
+    while (!l->need_stop) {
+    
+    }
 }
 
 int cio_event_loop_stop(void *loop)
