509c28126d0ce7c71d19f49937a0c085d14ab72b
Александр Куликов <rippa80@gmail.com>
2018-09-23 23:15:06 +0300

    Go on with connection UT.

--- test/src/tcp_connection_ut.c
+++ test/src/tcp_connection_ut.c
@@ -9,28 +9,116 @@
 #include <errno.h>
 #include <unistd.h>
 
-/**
- * Test server code.
- */
-struct test_server_ctx {
-    void *connection;
-    void *acceptor;
-    char read_buf[BUFSIZ];
-    char write_buf[BUFSIZ];
+struct growable_buffer {
+    char *data;
+    int size;
+    int capacity;
 };
 
-static void free_server_ctx(struct test_server_ctx *server_ctx)
+void free_growable_buffer(struct growable_buffer *growable_buffer)
+{
+    if (growable_buffer) {
+        free(growable_buffer->data);
+        free(growable_buffer);
+    }
+}
+
+struct growable_buffer *new_growable_buffer(int capacity)
+{
+    struct growable_buffer *growable_buffer;
+    
+    if (!(growable_buffer = malloc(sizeof(*growable_buffer))))
+        goto fail;
+    
+    if (!(growable_buffer->data = malloc(capacity)))
+        goto fail;
+    
+    growable_buffer->size = 0;
+    growable_buffer->capacity = capacity;
+    
+    return growable_buffer;
+    
+fail:
+    free_growable_buffer(growable_buffer);
+    return NULL;
+}
+
+int growable_buffer_append(struct growable_buffer *growable_buffer, const void *data, int size)
+{
+    int new_capacity, new_size;
+    
+    new_capacity = growable_buffer->capacity;
+    new_size = growable_buffer->size + size;
+    if (new_size > new_capacity) {
+        while (new_size > new_capacity)
+            new_capacity *= 2;
+        if (!(growable_buffer->data = realloc(growable_buffer->data, new_capacity)))
+            return -1;
+    }
+    memcpy(growable_buffer->data + growable_buffer->size, data, size);
+    growable_buffer->size = new_size;
+    
+    return 0;
+}
+
+struct test_client {
+    void *connection;
+    int connected;
+    int written;
+    struct growable_buffer *read_buf;
+};
+
+static void free_test_client(struct test_client *test_client)
+{
+    if (test_client) {
+        cio_free_tcp_connection_sync(test_client->connection);
+        free_growable_buffer(test_client->read_buf);
+        free(test_client);
+    }
+}
+
+static struct test_client *new_test_client_ctx(void *event_loop, void *user_ctx,
+                                                   int create_connection)
+{
+    struct test_client *client_ctx;
+    
+    client_ctx = malloc(sizeof(*client_ctx));
+    if (!client_ctx)
+        goto fail;
+    
+    memset(client_ctx, 0, sizeof(*client_ctx));
+    if (create_connection
+            && !(client_ctx->connection = cio_new_tcp_connection(event_loop, user_ctx)))
+        goto fail;
+    
+    if (!(client_ctx->read_buf = new_growable_buffer(BUFSIZ)))
+        goto fail;
+    
+    return client_ctx;
+    
+fail:
+    free_test_client(client_ctx);
+    return NULL;
+}
+
+struct test_server {
+    struct test_client *server_client;
+    void *acceptor;
+    int accepted;
+};
+
+static void free_server_ctx(struct test_server *server_ctx)
 {
     if (server_ctx) {
-        cio_free_tcp_connection_sync(server_ctx->connection);
+        free_test_client(server_ctx->server_client);
         cio_free_tcp_acceptor_sync(server_ctx->acceptor);
         free(server_ctx);
     }
 }
 
-static struct test_server_ctx *new_test_server_ctx(void *event_loop, void *user_ctx)
+static struct test_server *new_test_server_ctx(void *event_loop, void *user_ctx)
 {
-    struct test_server_ctx *server_ctx;
+    struct test_server *server_ctx;
     
     if (!(server_ctx = malloc(sizeof(*server_ctx))))
         goto fail;
@@ -39,6 +127,9 @@ static struct test_server_ctx *new_test_server_ctx(void *event_loop, void *user_
     if (!(server_ctx->acceptor = cio_new_tcp_acceptor(event_loop, user_ctx)))
         goto fail;
     
+    if (!(server_ctx->server_client = new_test_client_ctx(event_loop, server_ctx->server_client, 0)))
+        goto fail;
+
     return server_ctx;
     
 fail:
@@ -50,14 +141,13 @@ fail:
  * Tests context.
  */
 struct connection_tests_ctx {
-    void *connection;
-    struct test_server_ctx *test_server_ctx;
+    struct test_server *test_server_ctx;
+    struct test_client *test_client_ctx;
     void *event_loop;
     pthread_t event_loop_thread;
-    int accepted;
-    int connected;
-    int duplex_on;
     pthread_mutex_t mutex;
+    int duplex_on;
+    char *test_data;
 };
 
 static const char *const VALID_SERVER_ADDR = "0.0.0.0";
@@ -68,12 +158,13 @@ static void free_connection_tests_ctx(struct connection_tests_ctx *test_ctx)
     void *result;
 
     if (test_ctx) {
-        cio_free_tcp_connection_sync(test_ctx->connection);
+        free_test_client(test_ctx->test_client_ctx);
         free_server_ctx(test_ctx->test_server_ctx);
         cio_event_loop_stop(test_ctx->event_loop);
         ASSERT_EQ_INT(0, pthread_join(test_ctx->event_loop_thread, &result));
         cio_free_event_loop(test_ctx->event_loop);
         pthread_mutex_destroy(&test_ctx->mutex);
+        free(test_ctx->test_data);
         free(test_ctx);
     }
 }
@@ -81,6 +172,22 @@ static void free_connection_tests_ctx(struct connection_tests_ctx *test_ctx)
 static void *event_loop_run_func(void *ctx)
 {
     return (void *) cio_event_loop_run(ctx);
+}
+
+static void *generate_test_data()
+{
+    const int data_size = 1024 * 1024 * 10;
+    char *data;
+    int i;
+    
+    if (!(data = malloc(data_size)))
+        return NULL;
+    
+    srand(time(NULL));
+    for (i = 0; i < data_size; ++i)
+        data[i] = rand() % 256;
+    
+    return data;
 }
 
 static struct connection_tests_ctx *new_connection_tests_ctx()
@@ -95,11 +202,15 @@ static struct connection_tests_ctx *new_connection_tests_ctx()
     memset(test_ctx, 0, sizeof(*test_ctx));
     if (!(test_ctx->event_loop = cio_new_event_loop(1024)))
         goto fail;
-    
-    if (!(test_ctx->connection = cio_new_tcp_connection(test_ctx->event_loop, test_ctx)))
+
+
+    if (!(test_ctx->test_client_ctx = new_test_client_ctx(test_ctx->event_loop, test_ctx, 1)))
         goto fail;
     
     if (!(test_ctx->test_server_ctx = new_test_server_ctx(test_ctx->event_loop, test_ctx)))
+        goto fail;
+    
+    if (!(test_ctx->test_data = generate_test_data()))
         goto fail;
 
     test_ctx->mutex = (pthread_mutex_t) PTHREAD_MUTEX_INITIALIZER;
@@ -142,11 +253,11 @@ static void on_accept(int fd, void *ctx, int ecode)
     struct connection_tests_ctx *tests_ctx = ctx;
     ASSERT_EQ_INT(ecode, CIO_NO_ERROR);
     ASSERT_NE_INT(fd, -1);
-    tests_ctx->test_server_ctx->connection = cio_new_tcp_connection_connected_fd(
+    tests_ctx->test_server_ctx->server_client->connection = cio_new_tcp_connection_connected_fd(
         tests_ctx->event_loop, tests_ctx, fd);
-    ASSERT_NE_PTR(NULL, tests_ctx->test_server_ctx->connection);
+    ASSERT_NE_PTR(NULL, tests_ctx->test_server_ctx->server_client->connection);
     pthread_mutex_lock(&tests_ctx->mutex);
-    tests_ctx->accepted = 1;
+    tests_ctx->test_server_ctx->accepted = 1;
     pthread_mutex_unlock(&tests_ctx->mutex);
 }
 
@@ -155,7 +266,7 @@ static void on_connect(void *ctx, int ecode)
     struct connection_tests_ctx *tests_ctx = ctx;
     ASSERT_EQ_INT(ecode, CIO_NO_ERROR);
     pthread_mutex_lock(&tests_ctx->mutex);
-    tests_ctx->connected = 1;
+    tests_ctx->test_client_ctx->connected = 1;
     pthread_mutex_unlock(&tests_ctx->mutex);
 }
 
@@ -168,7 +279,7 @@ static void when_echo_tcp_server_started(struct connection_tests_ctx *tests_ctx,
 static void when_connection_attempt_is_made(struct connection_tests_ctx *tests_ctx,
                                             const char *addr, int port)
 {
-    cio_tcp_connection_async_connect(tests_ctx->connection, addr, port, on_connect);
+    cio_tcp_connection_async_connect(tests_ctx->test_client_ctx->connection, addr, port, on_connect);
 }
 
 static void when_duplex_mode_is(struct connection_tests_ctx *tests_ctx, int on)
@@ -180,7 +291,7 @@ static void then_both_side_connections_are_successful(struct connection_tests_ct
 {
     while (1) {
         pthread_mutex_lock(&tests_ctx->mutex);
-        if (tests_ctx->accepted == 1 && tests_ctx->connected == 1) {
+        if (tests_ctx->test_server_ctx->accepted == 1 && tests_ctx->test_client_ctx->connected == 1) {
             pthread_mutex_unlock(&tests_ctx->mutex);
             break;
         }
@@ -191,7 +302,10 @@ static void then_both_side_connections_are_successful(struct connection_tests_ct
 
 static void when_data_transfer_is_started(struct connection_tests_ctx *tests_ctx)
 {
+    ASSERT_NE_PTR(NULL, tests_ctx->test_client_ctx->connection);
+    ASSERT_NE_PTR(NULL, tests_ctx->test_server_ctx->server_client->connection);
     
+    //cio_tcp_connection_async_write(tests_ctx->test_client_ctx->connection, )
 }
 
 static void then_all_data_transferred_correctly(struct connection_tests_ctx *tests_ctx)
@@ -204,7 +318,7 @@ static void then_all_data_transferred_correctly(struct connection_tests_ctx *tes
  */
 void test_new_tcp_connection(void **ctx)
 {
-    ASSERT_NE_PTR(NULL, ((struct connection_tests_ctx *)(*ctx))->connection);
+    ASSERT_NE_PTR(NULL, ((struct connection_tests_ctx *)(*ctx))->test_client_ctx->connection);
 }
 
 void test_tcp_connection_connect_correct_address(void **ctx)
